381ffb7bfdaf47872a84b3815383670a093f3843
Shravan <ishravan919@gmail.com>
2022-07-19 12:53:48 +0530

    Prefix Bug Fixed

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const path = require("path");
 
 
 // config file
-if(process.env.NODE_ENV!=="PRODUCTION"){
+if(process.env.NODE_ENV !== "PRODUCTION"){
   require("dotenv").config({path:"backend/config/config.env"});
   }
   
