57a5db437508bacab51bc5be975b250203f047bf
Shravan <ishravan919@gmail.com>
2023-06-24 22:09:22 +0530

    deploy

--- backend/app.js
+++ backend/app.js
@@ -32,18 +32,13 @@ app.use("/api/v1",user);
 app.use("/api/v1",order);
 app.use("/api/v1",payment);
 
-// Your code
-if (process.env.NODE_ENV === "production") {
+
+if (process.env.NODE_ENV === "PRODUCTION") {
   app.use(express.static(path.join(__dirname,"../frontend/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
   });
 }
-// Your code
-
-
-
-
 
 // app.use(express.static(path.join(__dirname,"../frontend/build")));
 
