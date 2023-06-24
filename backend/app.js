76e515f53cccf925f25d0f085374b571aea8e1f1
Shravan <ishravan919@gmail.com>
2023-06-24 20:21:57 +0530

    deploy

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,6 @@ app.use("/api/v1",order);
 app.use("/api/v1",payment);
 // Your code
 if (process.env.NODE_ENV === "production") {
-  const path = require("path");
   app.use(express.static(path.join(__dirname,"../frontend/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
